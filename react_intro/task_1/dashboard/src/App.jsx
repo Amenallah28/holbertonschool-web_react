c428c9713e4c96e4d085a4cb3c59638e2f303f62
Amenallah28 <amenallahmrabet9@gmail.com>
2025-01-03 14:26:01 +0100

    task 1

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import holbertonLogo from './assets/holberton-logo.jpg';
+import { getCurrentYear , getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>&copy; {getCurrentYear()} {getFooterCopy(false)} </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
