a807480cbf55baf6ea8ecc2d66d545262592350a
Amenallah28 <amenallahmrabet9@gmail.com>
2025-01-03 18:26:14 +0100

    tests

--- react_intro/task_3/dashboard/src/utils.spec.js
+++ react_intro/task_3/dashboard/src/utils.spec.js
@@ -1,21 +1,16 @@
 import "./utils"
-import { getCurrentYear, getFooterCopy, getLatestNotification } from "./utils"
+import { getCurrentYear, getFooterCopy, getLatestNotification} from "./utils"
 
-test ("getCurrentYear should return the current year", () => {
-    const currentYear = new Date().getFullYear();
-    expect(getCurrentYear()).toBe(currentYear);
-});
+test("testing the getCurrentYear function", () => {
+    const CurrentYear = new Date
+    expect(getCurrentYear()).toBe(CurrentYear.getFullYear())
+})
 
-test("getFooterCopy should return 'Holberton School' when argument is true", () => {
-    expect(getFooterCopy(true)).toBe("Holberton School");
-  });
-  
-  test("getFooterCopy should return 'Holberton School main dashboard' when argument is false", () => {
-    expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
-  });
-  
-  test("getLatestNotification should return the correct notification string", () => {
-    const expectedString = "<strong>Urgent requirement</strong> - complete by EOD";
-    expect(getLatestNotification()).toBe(expectedString);
-  });
-  
+test("testing the getFooterCopy function", () => {
+    expect(getFooterCopy(true)).toBe("Holberton School")
+    expect(getFooterCopy(false)).toBe("Holberton School main dashboard")
+})
+
+test("testing the getLatestNotification function", () => {
+    expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD")
+})
