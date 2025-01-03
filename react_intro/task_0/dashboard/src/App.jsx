98a39bb88b7759f5df717186111e0aa5ec6b6f73
Amenallah28 <amenallahmrabet9@gmail.com>
2025-01-03 11:54:01 +0100

    task 0

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,17 +1,22 @@
-
-import './App.css'
+import { useState } from 'react'
+import './App.css';
+import holbertonLogo from './assets/holberton-logo.jpg';
 
 function App() {
   return (
-    <>
-      <div className='App-header' alt="holberton logo">
-        <img src="src/assets/holberton-logo.jpg" alt="holberton logo" ></img>
-        <h1 role="heading">School dashboard</h1>
+    <div>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
-      <div className='App-body'><p>Login to access the full dashboard</p></div>
-      <div className='App-footer'><p>Copyright 2024 - holberton School</p></div>
-    </>
-  )
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2024 - Holberton School</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
