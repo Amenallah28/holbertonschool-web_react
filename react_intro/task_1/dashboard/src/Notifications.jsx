1bcd4176df083796ce3a9c4e1d425e667500cbe4
Amenallah28 <amenallahmrabet9@gmail.com>
2025-01-03 16:03:10 +0100

    task 1

--- react_intro/task_1/dashboard/src/Notifications.jsx
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-import './Notifications.css';
+import "./Notifications.css"
 
 function Notifications() {
-    return (
-        <div className="notifications">
-            <p>Here is the list of notifications</p>
-        </div>
-    );
+    return (<div className="notifications"><p>Here is the list of notifications</p></div>)
 }
-export default Notifications;
+
+export default Notifications
